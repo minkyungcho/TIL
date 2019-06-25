36f5b49e72278772a0f6df0fac9ab5019590173f
Ming Cho <xiuperjjo@naver.com>
2019-06-25 19:02:39 +0900

    #11654

---
+++ 190625/AlgoStudy/c1.java
@@ -0,0 +1,15 @@
+package day02;
+
+import java.util.Scanner;
+
+public class c1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextLine().charAt(0);
+
+		System.out.println(n);
+	}
+
+}
