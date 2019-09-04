5d30d631f69cc22ffdb7abbd6b9969a6fa13f602
minkyungcho <xiuperjjo@naver.com>
2019-09-04 09:10:44 +0900

    P474

---
+++ Android/day06/P474/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.p474;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView, textView2;
+    Button button, button2;
+    boolean flag1=true, flag2=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+    }
+    Runnable r1 = new Runnable() {
+        @Override
+        public void run() {
+            for(int i=1; i<=10; i++){
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("[T]","####"+i);
+                final int temp = i;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(temp+"");
+                    }
+                });
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    button.setEnabled(true);
+                }
+            });
+        }
+    };
+    Runnable r2 = new Runnable() {
+        @Override
+        public void run() {
+            for(int i=11; i<=20; i++){
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.d("[T]","****"+i);
+                final int temp = i;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView2.setText(temp +"");
+                    }
+                });
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    button2.setEnabled(true);
+                }
+            });
+        }
+    };
+
+    public void clickBt1(View view){
+        Thread t1 = new Thread(r1);
+        t1.start();
+        button.setEnabled(false);
+    }
+    public void clickBt2(View view){
+        Thread t2 = new Thread(r2);
+        t2.start();
+        button2.setEnabled(false);
+    }
+}
