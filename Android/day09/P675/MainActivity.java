6e962bf2797249ea443740073025b370f965bd3e
minkyungcho <xiuperjjo@naver.com>
2019-09-11 17:31:05 +0900

    p675

---
+++ Android/day09/P675/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.p675;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity {
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    MarkerOptions myLocationMarker;
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.d("Map", "지도 준비됨.");
+                map = googleMap;
+                map.setMyLocationEnabled(true);
+                startLocationService();
+            }
+        });
+
+        try {
+            MapsInitializer.initialize(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String[] permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+        ActivityCompat.requestPermissions(this, permissions, 101);
+        int check =
+                PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(check == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,
+                    "ACCEPT", Toast.LENGTH_SHORT).show();
+
+            return;
+        }else{
+            Toast.makeText(this,
+                    "DENY", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+    } // end onCreate
+
+    private void showCurrentLocation(Double latitude, Double longitude) {
+        LatLng curPoint = new LatLng(latitude, longitude);
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint, 15));
+        showMyLocationMarker(curPoint);
+    }
+    private void showMyLocationMarker(LatLng curPoint) {
+        if (myLocationMarker == null) {
+            myLocationMarker = new MarkerOptions();
+            myLocationMarker.position(curPoint);
+            myLocationMarker.title("● 내 위치\n");
+            myLocationMarker.snippet("● GPS로 확인한 위치");
+            myLocationMarker.icon(BitmapDescriptorFactory.fromResource(R.drawable.mylocation));
+            map.addMarker(myLocationMarker);
+        } else {
+            myLocationMarker.position(curPoint);
+        }
+    }
+
+    public void startLocationService() {
+        LocationManager manager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String message = "" + latitude + ":" + longitude;
+                showCurrentLocation(latitude,longitude);
+                textView.setText(message);
+            }
+
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 1;
+            float minDistance = 0;
+
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime, minDistance, gpsListener);
+
+            Toast.makeText(getApplicationContext(), "",
+                    Toast.LENGTH_SHORT).show();
+
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+            showCurrentLocation(latitude,longitude);
+            String message = ""+ latitude + ":"+ longitude;
+            textView.setText(message);
+        }
+
+        public void onProviderDisabled(String provider) { }
+
+        public void onProviderEnabled(String provider) { }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
+    }
+
+
+}
