3929055c71406b63fb3cc92fdd78ccd99d75197e
ming_Mac <xiuperjjo@naver.com>
2019-11-27 19:09:46 +0900

    add day16 CRUD(AJAX), Auth

---
+++ Django/day15/instagram/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views as accounts_views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', accounts_views.login, name="login"),
+    path('logout/', accounts_views.logout, name="logout"),
+    path('signup/', accounts_views.signup, name="signup"),
+]
---
+++ Django/day15/instagram/article/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views as article_views
+
+urlpatterns = [
+    path('', article_views.index, name="articles"),
+    # JS 
+    path('js-test', article_views.js_test),
+    # JQ
+    path('jq-test', article_views.jq_test),
+    path('jq-test/boards/', article_views.submit_boards, name="submit_boards"),
+    path('jq-test/boards/edit', article_views.edit_boards, name="edit_boards"),
+    path('jq-test/boards/delete', article_views.delete_boards, name="delete_boards"),
+    
+    path('articles/<int:article_id>/delete', article_views.delete, name="delete"),
+    path('articles/<int:article_id>/edit', article_views.edit, name="edit"),
+
+    path('comments/', article_views.comments, name="comments"),
+    path('comments/delete', article_views.delete_comments, name="delete_comments"),
+    path('comments/<int:comment_id>/edit', article_views.edit_comments, name="edit_comments"),
+]
